bd405d8309a63d0fef3cbc9aef2ad6fedef327e3
adamrice123 <rice-a15@ulster.ac.uk>
2021-04-28 13:39:34 +0100

    DeleteSalary Changes

--- Admin/DeleteSalary.aspx.cs
+++ Admin/DeleteSalary.aspx.cs
@@ -1,14 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
-
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+using System.IO;
 public partial class Admin_DeleteSalary : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        String CS = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(CS))
+        {
 
+            string Id = Request.QueryString["Id"].ToString();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete from SalaryTable where EmployeeId='" + Id + "'", con);
+            cmd.ExecuteNonQuery(); ;
+            Response.Redirect("~/Admin/ViewSalary.aspx");
+        }
     }
 }
